6037ee152493227205b2c0e19c168dc22e2e87ce
ChannXhann <channchannofficial@gmail.com>
2024-12-06 11:45:59 +0700

    Update

--- controller/verifikasi_anggota.php
+++ controller/verifikasi_anggota.php
@@ -16,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nik_anggota'])) {
 
         if ($stmt->rowCount() > 0) {
             // Jika berhasil, tampilkan pesan sukses dan hentikan eksekusi
-            echo "<script>alert('Status verifikasi berhasil diubah menjadi Disetujui dan tanggal pendaftaran diperbarui.'); window.location.href='../../view/pages_super/lihat_data_anggota.php';</script>";
+            echo "<script>alert('Status verifikasi berhasil diubah menjadi Disetujui dan tanggal pendaftaran diperbarui.'); window.location.href='../../view/pages_super/data_anggota.php';</script>";
             exit;
         } else {
             // Jika tidak ada baris yang diperbarui, tampilkan pesan gagal dan hentikan eksekusi
@@ -30,7 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nik_anggota'])) {
     }
 } else {
     // Jika akses tidak valid, tampilkan pesan dan hentikan eksekusi
-    echo "<script>alert('Akses tidak valid.'); window.location.href='../../view/pages_super/lihat_data_anggota.php';</script>";
+    echo "<script>alert('Akses tidak valid.'); window.location.href='../../view/pages_super/data_anggota.php';</script>";
     exit;
 }
 ?>
